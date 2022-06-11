65c1f54bb37c95b89a367b596d129cb91e6777d8
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-06-11 23:23:26 +0100

    installing tailwind css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <div className=" fixed z-50 w-screen bg-slate-300 p-6 px-16 ">
-    Header
+    {/*Destop*/}
+    <div className="hidden md:flex w-full h-full bg-red-600 "></div>
+    {/*mobile*/}
+    <div className="flex md:hidden w-full h-full bg-blue-600 " ></div>
     </div>
   )
 }
