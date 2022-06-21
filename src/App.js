630915a9798bb5bedb6ee57ab5a486513fda959c
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-06-21 05:34:57 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 import { Header } from './components'
+import {MainContainer} from './components'
+import { CreateContainer } from './components'
 
 const App = () => { return (
-    <div className="w-screen h-auto flex flex-col">
+    <AnimatePresence>
+    <div className="w-screen h-auto flex flex-col bg-primary">
     <Header/>
-    </div> )
+
+    <main className='mt-24 p-8 w-full'>
+    <Routes>
+    <Route path='/*' element={<MainContainer />}/>
+    <Route path='/createItem' element={<CreateContainer />}/>
+
+    </Routes>
+     </main>
+    </div>
+    </AnimatePresence>
+ )
+
 }
 
 export default App
