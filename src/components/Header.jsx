b71cacd27f8ce292c120c1ded94ee15ee5448b09
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-06-21 05:35:07 +0100

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+import {MdShoppingBasket} from 'react-icons/md';
+
+import Logo from '../img/logo.png'
+import Avatar from "../img/avatar.png";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className=" fixed z-50 w-screen bg-slate-300 p-6 px-16 ">
+    <div className=" fixed z-50 w-screen p-6 px-16 ">
     {/*Destop*/}
-    <div className="hidden md:flex w-full h-full bg-red-600 "></div>
+    <div className="hidden md:flex w-full h-full items-center justify-between">
+    <Link to={"/"} className= "flex item-center gap-2">
+    <img src={Logo} alt="logo" className='w-8 object-cover'></img>
+    <p className='text-headingColor text-xl font-bold'>City</p>
+    </Link>
+
+    <div className='flex items-center gap-8'>
+    <ul className="flex items-center gap-8  ">
+    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
+    Home</li>
+    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
+    Menu</li>
+    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
+    About Us</li>
+    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
+    Serives</li>
+    </ul>
+
+    <div className='relative flex items-center justify-center'>
+    <MdShoppingBasket className='text-textColor text-2xl ml-8 cursor-pointer' />
+    <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex
+    items-center justify-center'>
+    <p className='text-xs text-white font-semibold'>2</p>
+    </div>
+    </div>
+    <motion.img whileTap={{scale: 0.6}}  src={Avatar} alt="user_img" className="w-10 min-w-[40px] h-10 min-h-[40px]
+     drop-shadow-x cursor-pointer" ></motion.img>
+    </div>
+    </div>
     {/*mobile*/}
     <div className="flex md:hidden w-full h-full bg-blue-600 " ></div>
     </div>
