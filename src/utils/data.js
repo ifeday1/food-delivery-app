cb2427836f0d9e585d679b14c6a0e5821ac1a66f
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-06-25 05:03:01 +0100

    Adding content to the header

---
+++ src/utils/data.js
@@ -0,0 +1,13 @@
+import i1 from "../img/i1.png";
+import f1 from "../img/f1.png";
+import c3 from "../img/c3.png";
+import fi1 from "../img/fi1.png";
+
+export const heroDate = [
+    { id: 1, name: 'Icecream', desp: 'Chocolate & Vanila', price: '6.99', imageSrc: i1 },
+    { id: 2, name: 'Strawberriees', desp: 'Fresh Strawberries', price: '15.99', imageSrc: f1 },
+    { id: 3, name: 'Chicken Kabab', desp: 'Mixed Kabab Plate', price: '10.99', imageSrc: c3 },
+    { id: 4, name: 'Fish Kabab', desp: 'Mixed Fieh Kabab', price: '8.99', imageSrc: fi1 },
+
+]
+export default heroDate;
